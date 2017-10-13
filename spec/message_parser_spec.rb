fea51688c0449eb4522af181cfb7ca899dac2756
derP@765pro <Giratina493@mew151.net>
2017-10-13 13:18:09 -0700

    MessageParser

--- spec/message_parser_spec.rb
+++ spec/message_parser_spec.rb
@@ -1,9 +1,31 @@
 require_relative '../src/message_parser'
 
 describe MessageParser do
-	describe '.new' do
-		it "is creatable" do
-			MessageParser.new
+	before :each do
+		@parser = MessageParser.new ( File.open(File.expand_path(File.dirname(__FILE__))+"/mockdata/chat_sample.txt", "rb").read )
+	end
+	describe '.parse' do
+		it "should get message text" do
+			messages = @parser.parse
+			expect(messages[0].message).to eq "https://bobuck.bandcamp.com/album/left-with-the-fires"
+			expect(messages[2].message).to eq "Oh boy, a NEW Residents newsletter from Cherry Red"
+		end
+		it "should get nicks correctly" do
+			messages = @parser.parse
+			expect(messages[0].author).to eq "snorp"
+			expect(messages[2].author).to eq "mewee"
+		end
+		it "should parse emojis correctly" do
+			messages = @parser.parse
+			expect(messages[7].message).to eq ":mrwonderfulscream:"
+		end
+		it "should get time correctly" do
+			messages = @parser.parse
+			expect(messages[0].time).to eq "08:35:37"
+		end
+		it "should get message ID" do
+			messages = @parser.parse
+			expect(messages[0].id).to eq 214981
 		end
 	end
 end
--- src/message_parser.rb
+++ src/message_parser.rb
@@ -1,2 +1,38 @@
 class MessageParser
+	Message = Struct.new("Message",:id,:time,:author,:message)
+	def initialize(htmldata)
+		@data = htmldata
+	end
+
+	def convert_emojis(msg)
+		emojiTemplate = /<img (.+?) src="(emoji|stickers)\/(.+?)\.png" (.+?) \/>/i
+		msg.gsub(emojiTemplate,":\\3:");
+	end
+
+	def clean_message(msg)
+		msg.gsub!(/<\/?span>/i,"");
+		msg = CGI.unescapeHTML(msg)
+		msg = convert_emojis(msg)
+		msg = clean_urls(msg)
+		return msg.strip
+	end
+
+	def clean_timestamp(time)
+		pieces = time.split(" ")
+		return pieces[1][0..7]
+	end
+
+	def clean_urls(msg)
+		urlTemplate = /<a href="(.+?)" target="_blank">\1<\/a>/im
+		pageCardTemplate = /<a href="(.+?)" target="_blank"><div class="messageLink">(.+?)<\/div><\/a>/im
+		return msg.gsub(urlTemplate,"\\1").gsub(pageCardTemplate,"")
+	end
+
+	def parse
+		msgTemplate = /<div class="chatLine" id="message_(\d+?)"><span class="picBox"><img class="profilePic" src=".+?"\/><\/span><span class="message"><span class="date" data-localtime-format>(.+?)<\/span><br \/><span class="userName( adminName)?">(.+?)<\/span>: (.+?)<\/span><\/div>/im
+		results = @data.scan(msgTemplate)
+		return results.map do |message|
+			Message.new(message[0].to_i,clean_timestamp(message[1]),message[3],clean_message(message[4]))
+		end
+	end
 end
