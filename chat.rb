54ef4722cb0e56e1fb4c20db5f346807ed6861a0
derP@765pro <Giratina493@mew151.net>
2017-10-19 15:54:46 -0700

    Removed OpenStruct from main

--- chat.rb
+++ chat.rb
@@ -2,7 +2,6 @@
 require_relative 'src/login/mock_login_server'
 require_relative 'src/login/console_login_prompt'
 require_relative 'src/ui/chat_ui'
-require 'ostruct'
 
 login_prompt = ConsoleLoginPrompt.new(MockLoginServer)
 userdata = login_prompt.run
