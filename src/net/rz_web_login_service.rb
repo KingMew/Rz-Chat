4c11360cc437680dfa355b89ed3835901a76eae1
meisekimiu <giratina493@mew151.net>
2020-10-23 09:19:52 -0700

    Update how PHPSESSID is gotten from chat login.

--- src/net/rz_web_login_service.rb
+++ src/net/rz_web_login_service.rb
@@ -8,7 +8,30 @@ class RzWebLoginService
 		@password = pw
 	end
 
+	def get_sessionid
+		uri = URI("https://www.residents.com/fans/chat/chat.php")
+		https = Net::HTTP.new(uri.host, uri.port)
+		https.use_ssl = true
+		sessionID = ""
+		response = https.get(uri.path)
+		if response.code != '200'
+			raise "Can't connect to RZ Chat Page"
+		else
+			response.get_fields('Set-Cookie').each do |cookie|
+				if cookie.include? "PHPSESSID"
+					return cookie.split(";")[0].split("=")[1]
+				end
+			end
+		end
+		return false
+	end
+
 	def do_login
+		sessionId = get_sessionid
+		if sessionId == false
+			raise "Can't get session id"
+		end
+		cookie = CGI::Cookie.new("PHPSESSID",sessionId)
 		uri = URI("https://www.residents.com/fans/chat/login.php")
 		https = Net::HTTP.new(uri.host, uri.port)
 		data = {
@@ -16,14 +39,16 @@ class RzWebLoginService
 			"userpass" => @password
 		}
 		https.use_ssl = true
-		sessionID = ""
-		response = https.post(uri.path,URI.encode_www_form(data))
+		request = Net::HTTP::Post.new(uri.request_uri)
+		request.set_form_data(data)
+		request['User-Agent'] = "Rz-Chat Terminal Client (https://github.com/meisekimiu/Rz-Chat)"
+		request['Cookie'] = cookie.to_s.sub(/; path=$/, '')
+		response = https.request(request)
 		if response.code != '200'
 			raise "Can't connect to RZ Chat Login"
 		else
 			if response.body == "successful login"
-				sessionId = response.get_fields('Set-Cookie')[0].split(";")[0]
-				return sessionId.split("=")[1]
+				return sessionId
 			else
 				p response.body
 				return false
